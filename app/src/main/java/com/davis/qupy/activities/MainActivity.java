24307229c894dc62664f815b92cc1e88f0316aed
Davisdavy <davisdavy96@gmail.com>
2020-11-29 02:18:12 +0300

    Subscribe into view model in the main activity

--- app/src/main/java/com/davis/qupy/activities/MainActivity.java
+++ app/src/main/java/com/davis/qupy/activities/MainActivity.java
@@ -1,16 +1,33 @@
 package com.davis.qupy.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.davis.qupy.R;
+import com.davis.qupy.viewmodels.MostPopularTVShowsViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MostPopularTVShowsViewModel viewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewModel = new ViewModelProvider(this).get(MostPopularTVShowsViewModel.class);
+        getMostPopularTVShows();
+    }
+
+    private void getMostPopularTVShows() {
+        viewModel.getMostPopularTVShows(0).observe(this, mostPopularTVShowsResponse ->
+                Toast.makeText(
+                        getApplicationContext(), "Total Pages: " + mostPopularTVShowsResponse.getTotalPages(),
+                        Toast.LENGTH_SHORT
+                ).show()
+        );
     }
 }
